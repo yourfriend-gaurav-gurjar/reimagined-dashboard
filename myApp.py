6e0a9571c11a9d530e47bad2eae2c23eb4228a43
yourfriend-gaurav-gurjar <gaurav@metteyyaanalyticsllc.com>
2021-04-21 19:02:45 +0530

    first draft of the script

---
+++ myApp.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import psycopg2 
+import pandas as pd
+
+@st.cache(show_spinner=False)
+def _load_data(filepath): 
+    return pd.read_csv(filepath)
+
+
+# Database connection
+con = None
+try:
+    con = psycopg2.connect(database='dbc', user='postgres',
+                           password='postgres')
+    cur = con.cursor()
+    st.write("""
+            # Business Leads
+        """)
+    st.text('List')
+    with st.beta_expander('Prospects', expanded=True):
+        how_to_load = st.selectbox('Countries List', ('Canada', 'USA'))
+        if how_to_load == 'Canada':
+            df = pd.read_sql_query("SELECT * FROM canada_01_raw WHERE first = 'Kyle'", con)
+            df[['city1', 'state1']] = df.location1.str.split(",",expand=True)
+#            df[['city2', 'state2']] = df.location2.str.split(",",expand=True)
+            df.drop(['unknown','location1', 'location2','time1', 'time2'],axis = 1, inplace = True)
+        if how_to_load == 'USA':
+            df = pd.read_sql_query('SELECT * FROM usa_01_raw', con)           
+            #df['city1', 'state1'] = df.location1.str.split(",", expand=True)            
+    if df is not None:
+        with st.beta_expander('Data Preview', expanded=True):
+            with st.spinner('Loading Data...'):
+                st.dataframe(df)
+                st.write('`{}` rows, `{}` columns'.format(df.shape[0],df.shape[1]))
+
+except psycopg2.DatabaseError as e:
+    print(f'Error {e}')
+    sys.exit(1)
+
+finally:
+    if con:
+        con.close()
